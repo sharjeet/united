f756bfeafa2d23f8968a28cc640c14d170eb5011
sharjeet <harjeet90.s@gmail.com>
2022-08-08 16:25:42 +0530

    Update map.js

--- assets/vendor/map.js
+++ assets/vendor/map.js
@@ -9,7 +9,7 @@ function initMap() {
 [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
 // End map style
 
-  var myLatlng = new google.maps.LatLng(-37.817251, 144.955891); // Set coordinates.
+  var myLatlng = new google.maps.LatLng(22.639594931567355, 88.42779388940976); // Set coordinates.
   var mapOptions = {
     mapTypeControl: true,
     scrollwheel: false,
